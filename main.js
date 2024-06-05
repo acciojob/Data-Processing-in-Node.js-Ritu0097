ac08bf59894b9cc4fa945693b27f5f19118fb968
Ritu0097 <rituchaudhary0097@gmail.com>
2024-06-05 14:35:05 +0530

    data

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-Implement the processData function in app.js:
-
 const fs = require('fs');
 const { Transform } = require('stream');
 
+class ProcessDataStream extends Transform {
+  constructor() {
+    super();
+  }
+
+  _transform(chunk, encoding, callback) {
+    // Process the data
+    const processedChunk = chunk.toString().toUpperCase();
+
+    // Push the processed data to the output stream
+    this.push(processedChunk);
+
+    // Call the callback to indicate that this chunk has been processed
+    callback();
+  }
+}
+
 function processData(inputFilePath, outputFilePath) {
-  // Implement this function
+  // Create a readable stream from the input file
+  const inputStream = fs.createReadStream(inputFilePath, { highWaterMark: 64 });
+
+  // Create a writable stream to the output file
+  const outputStream = fs.createWriteStream(outputFilePath);
+
+  // Create an instance of the ProcessDataStream
+  const processDataStream = new ProcessDataStream();
+
+  // Pipe the input stream through the transform stream and into the output stream
+  inputStream
+    .pipe(processDataStream)
+    .pipe(outputStream)
+    .on('finish', () => {
+      console.log('Data processing completed successfully.');
+    })
+    .on('error', (err) => {
+      console.error('Error occurred during data processing:', err);
+    });
 }
 
 processData('input.txt', 'output.txt');
-
